367772fa60904fd0ad64589c0c47c15aaf275545
BarbaraPal <pal.barbi@gmail.com>
2022-08-30 21:00:48 +0200

    manjši popravki

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -101,31 +101,6 @@ diagram.kolena = function(k.visina) {
     theme_classic()
 }
 
-diagram.skupine = function(podatki, oznake, skupine, k) {
-  podatki = podatki %>%
-    bind_cols(skupine) %>%
-    rename(skupina = ...4)
-  d = podatki %>%
-    ggplot(
-      mapping = aes(
-        x = x, y = y, color = skupina
-      )
-    ) +
-    geom_point() +
-    geom_label(label = oznake, size = 2) +
-    scale_color_hue(h = c(180, 270)) +
-    xlab("") +
-    ylab("") +
-    theme_classic() 
-  for (i in 1:k) {
-    d = d + geom_encircle(
-      data = podatki %>%
-        filter(skupina == i)
-    )
-  }
-  d
-}
-
 obrisi = function(podatki, hc = TRUE, od = 2, do = NULL) {
   n = nrow(podatki)
   if (is.null(do)) {
@@ -210,7 +185,7 @@ r.km.plt <- diagram.obrisi(r.km)
 diagram.skupine = function(podatki, oznake, skupine, k) {
   podatki = podatki %>%
     bind_cols(skupine) %>%
-    rename(skupina = ...4)
+    dplyr::rename(skupina = ...4)
   
   d = podatki %>%
     ggplot(
